bcffcfd934259ad871f0cefc96ab8553cde39661
Andrey <dmitrich27@ukr.net>
2022-02-13 17:02:37 +0200

    Initial commit

---
+++ src/main/java/com/andreydm/spring/rest/App.java
@@ -0,0 +1,39 @@
+package com.andreydm.spring.rest;
+
+import com.andreydm.spring.rest.configuration.MyConfig;
+import com.andreydm.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+
+//        List<Employee> employeeList = communication.getAllEmployees();
+//        System.out.println(employeeList);
+
+
+//        Employee employee = communication.getEmployee(1);
+//        System.out.println(employee);
+
+//        Employee employee = new Employee();
+//        employee.setId(12);
+//        employee.setDepartment("HR");
+//        employee.setName("Jon");
+//        employee.setSalary(500);
+//        employee.setSurname("Petrov");
+//
+//        communication.saveEmployee(employee);
+
+        communication.deleteEmployee(1);
+    }
+}
